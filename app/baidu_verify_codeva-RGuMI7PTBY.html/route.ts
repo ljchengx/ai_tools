19f952ecbce1757bd0dadba8fae1d68ed15bbd44
chenglongjie <ljchengx@outlook.com>
2026-08-19 16:31:32 +0800

    fix: serve Baidu verification URL

---
+++ app/baidu_verify_codeva-RGuMI7PTBY.html/route.ts
@@ -0,0 +1,9 @@
+export const dynamic = "force-static";
+
+export function GET() {
+  return new Response("659ff0cd2b4ca2f565cbf1fa836d0c68\n", {
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+    },
+  });
+}
